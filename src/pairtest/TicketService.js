89f1f77b1528b4b3d828e842a96bcdc1145eb32a
burhan-u <46581621+burhan-u@users.noreply.github.com>
2023-06-21 02:12:32 +0100

    feat: add validation for number of tickets and type

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,6 +1,6 @@
 /* eslint-disable class-methods-use-this */
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketTypeRequest from './lib/TicketTypeRequest';
+import InvalidPurchaseException from './lib/InvalidPurchaseException';
 
 export default class TicketService {
   /**
@@ -10,6 +10,26 @@ export default class TicketService {
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
     this.#validateAccountID(accountId);
+
+    if (!ticketTypeRequests.length) {
+      throw new InvalidPurchaseException('Invalid ticket type');
+    }
+
+    ticketTypeRequests.forEach((ticket) => {
+      if (!(ticket instanceof TicketTypeRequest)) {
+        throw new InvalidPurchaseException('Invalid ticket type');
+      }
+    });
+
+    // eslint-disable-next-line arrow-body-style
+    const totalTickets = ticketTypeRequests.reduce((total, tickets) => {
+      return total + tickets.getNoOfTickets();
+    }, 0);
+
+    const MAX_TICKET_AMOUNT = 20;
+    if (totalTickets > MAX_TICKET_AMOUNT) {
+      throw new InvalidPurchaseException('Maximum of 20 tickets per purchase');
+    }
   }
 
   #validateAccountID(accountId) {
--- test/pairtest/TicketService.test.js
+++ test/pairtest/TicketService.test.js
@@ -42,4 +42,37 @@ describe('TicketService', () => {
       }).not.toThrow(invalidIdException);
     });
   });
+
+  describe('Ticket Type Requests validation', () => {
+    it('should throw exception if tickets are empty', () => {
+      expect(() => {
+        ticketService.purchaseTickets(1);
+      }).toThrow(new InvalidPurchaseException('Invalid ticket type'));
+    });
+
+    it('should throw an exception if tickets are of invalid type', () => {
+      const tickets = [{ ADULT: 5 }, { CHILD: 2 }];
+
+      expect(() => {
+        ticketService.purchaseTickets(1, tickets);
+      }).toThrow(new InvalidPurchaseException('Invalid ticket type'));
+    });
+
+    it.each([
+      [[new TicketTypeRequest('ADULT', 21)]],
+      [[new TicketTypeRequest('ADULT', 12), new TicketTypeRequest('CHILD', 10)]],
+    ])('should throw an exception if total number of tickets is greater than 20', (tickets) => {
+      expect(() => {
+        ticketService.purchaseTickets(1, ...tickets);
+      }).toThrow(new InvalidPurchaseException('Maximum of 20 tickets per purchase'));
+    });
+
+    it('should not throw an exception if tickets are valid', () => {
+      const tickets = new TicketTypeRequest('ADULT', 20);
+
+      expect(() => {
+        ticketService.purchaseTickets(1, tickets);
+      }).not.toThrow(new InvalidPurchaseException('Maximum of 20 tickets per purchase'));
+    });
+  });
 });
