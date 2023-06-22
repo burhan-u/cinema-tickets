02012025dc1e6b28e786b3ecf846ed4fafaade8d
burhan-u <46581621+burhan-u@users.noreply.github.com>
2023-06-22 12:12:00 +0100

    refactor: use value from config

--- src/pairtest/lib/ErrorMessages.js
+++ src/pairtest/lib/ErrorMessages.js
@@ -1,10 +1,12 @@
-const errorMessages = {
+import config from './TicketServiceConfig';
+
+const errorMessages = Object.freeze({
   invalidAccId: 'Invalid account ID',
   invalidTickets: 'Invalid ticket type',
   noTickets: 'No tickets provided',
   noAdult: 'Tickets cannot be purchased without an Adult ticket',
   maxInfants: 'Number of Infant tickets cannot be greater than number of Adult tickets',
-  maxTickets: 'Maximum of 20 tickets per purchase',
-};
+  maxTickets: `Maximum of ${config.maxTicketCount} tickets per purchase`,
+});
 
 export default errorMessages;
